76be6ce1cf8858b4b7e9b4328c9f98762f0c13e6
Thiago Silva <thiago.dasilvabarbosa@student.griffith.ie>
2025-07-23 12:09:29 +0100

    Developers updated in Main

---
+++ Assignment_4/js/scriptdeveloper.js
@@ -0,0 +1,60 @@
+const toggledStates = {
+  austine: false,
+  emmanuel: false,
+  thiago: false
+};
+
+function changeCardContent(character) {
+  const card = document.getElementById(`${character}-card`);
+  const image = card.querySelector('img');
+  const paragraph = card.querySelector('p');
+
+  if (!toggledStates[character]) {
+    if (character === 'austine') {
+      image.src = '/images/developers/austine/austine-yasuo.png';
+      paragraph.textContent = 'Austine loves the art of games. Since childhood, playing was a joy from his heart, and now the time is joyful like a night in the winds. He is skilled and praises his games, for giving joy that can be seen like the shaping of the world. May games never fade – they are part of his home and soul.';
+    } else if (character === 'emmanuel') {
+      image.src = '/images/developers/emmanuel/emmanuel-player.png';
+      paragraph.textContent = 'Emmanuel has always been passionate about games, with a special love for sports. From a young age in Nigeria, he found joy and excitement on the playing field, whether it was football, basketball, or athletics. Sports fuel his competitive spirit and bring people together, inspiring him to push his limits and foster teamwork wherever he plays.';
+    } else if (character === 'thiago') {
+      image.src = '/images/developers/thiago/thiago-mage.png';
+      paragraph.textContent = 'Thiago is deeply passionate about games, with a special love for role-playing games (RPGs). He enjoys immersing himself in rich stories and complex worlds where imagination and strategy come alive. For Thiago, RPGs are more than just games—they’re a way to explore different characters, build friendships, and experience epic adventures beyond the ordinary.';
+    }
+  } else {
+    if (character === 'austine') {
+      image.src = '/images/developers/austine/WhatsApp Image 2025-07-14 at 14.49.07.jpeg';
+      paragraph.textContent = 'A man born in the land of Eldamar. He is a dreamer and a beloved from the depths of the heart. His coming was in a bright moment, like stars shining. He is a brave and faithful man, who leaves the good to seek love in all darkness.';
+    } else if (character === 'emmanuel') {
+      image.src = '/images/developers/emmanuel/WhatsApp Image 2025-07-14 at 15.00.22.jpeg';
+      paragraph.textContent = 'Emmanuel is a young man from Nigeria, known for his warm spirit and strong determination. Growing up surrounded by vibrant culture and rich traditions, he carries with him a deep sense of pride and resilience. Passionate about learning and connecting with others, Emmanuel strives to make a positive impact wherever he goes.';
+    } else if (character === 'thiago') {
+      image.src = '/images/developers/thiago/WhatsApp Image 2025-07-12 at 22.12.31.jpeg';
+      paragraph.textContent = 'Thiago is a vibrant young man from Brazil, full of energy and creativity. Growing up amidst Brazil’s lively culture and beautiful landscapes, he developed a deep appreciation for music, football, and community. With a warm heart and a curious mind, Thiago embraces life with enthusiasm and a passion for new experiences';
+    }
+  }
+
+  toggledStates[character] = !toggledStates[character];
+}
+//---------------------------------------------------------------------------------------------------------------------
+document.addEventListener('DOMContentLoaded', function() {
+
+  const elements = document.querySelectorAll('.hidden-information, .hidden-right, .hidden-information2, .hidden');
+
+  window.addEventListener('scroll', () => {
+    elements.forEach(el => {
+      // To get the position of the element on top
+      const elementTop = el.getBoundingClientRect().top;
+      const windowHeight = window.innerHeight;
+
+      // If the top is at 2/3 from the windon it appears
+      if (elementTop < windowHeight * 0.66) {
+        // To add a class which make the element visible
+        el.classList.add('visible');
+      } else {
+        // To remove the class to have the same effect again
+        el.classList.remove('visible');
+      }
+    });
+  });
+  window.dispatchEvent(new Event('scroll'));
+});
